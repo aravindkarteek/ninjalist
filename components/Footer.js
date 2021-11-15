c696e8627cb1c29fe05c849cf93fcaef2787b145
aravindkarteek <aravindkarteek@gmail.com>
2021-11-15 17:17:37 +0530

    next js app

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyrights 2021 Ninja list</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja list | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+          necessitatibus! Consequuntur nemo necessitatibus incidunt accusamus et
+          consectetur voluptas a praesentium, alias, vero iusto unde porro ex
+          vitae? Quidem, aliquid maiores!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+          necessitatibus! Consequuntur nemo necessitatibus incidunt accusamus et
+          consectetur voluptas a praesentium, alias, vero iusto unde porro ex
+          vitae? Quidem, aliquid maiores!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
